ba2a2dd70b33b3e34f13bec4d48a89b465c43e79
Hyunki <sjy6149@gmail.com>
2024-07-27 20:49:50 +0900

    d

---
+++ app/about-us/company/jobs/sales/layout.tsx
@@ -0,0 +1,7 @@
+export default function SalseLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}$$$$$</div>;
+}
---
+++ components/movie-videos.tsx
@@ -0,0 +1,26 @@
+import { api_url } from "../app/constants";
+import styles from "../styles/movie-video.module.css"
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos : ${Date.now()}`);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${api_url}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideos(id);
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video}
+        />
+      ))}
+    </div>
+  );
+}
